ecfe784be6ea3026ffdca6f3e007c11fe28c5f6c
programmer-jafir <jafiranjum2020@gmail.com>
2022-04-04 10:18:27 +0600

    Navbar compleate

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <nav>
+                <Link to='/home'>Home</Link>
+                <Link to='/Review'>Review</Link>
+                <Link to='/dashboard'>Dashboard</Link>
+                <Link to='/blogs'>Blogs</Link>
+                <Link to='/about'>About</Link>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
